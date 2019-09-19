62c98aadf5274f99096d96689c8fd736fc199de5
akorshun <razor1702@gmail.com>
2019-09-19 22:34:40 +0300

    add main

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsatou <bsatou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/13 16:29:54 by bsatou            #+#    #+#             */
+/*   Updated: 2019/09/19 22:34:24 by bsatou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while (get_next_line(fd, &line))
+		{
+			printf("%s\n", line);
+			free(line);
+			line = NULL;
+		}
+		close(fd);
+	}
+	return (0);
+}
